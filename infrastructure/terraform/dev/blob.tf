24ed71ea53e5022f48e84547966b9093a283a099
Nazmy Achmad <nazmy.achmad@unity3.com>
2023-08-15 10:03:46 +0700

    [khi-341] provision azure storage account for misc and container for cloud_shell

---
+++ infrastructure/terraform/dev/blob.tf
@@ -0,0 +1,16 @@
+# storage for client misc (cloud bash etc)
+resource "azurerm_storage_account" "misc_storage" {
+  name                = replace("${var.resource_prefix}-mis-storage", "-", "")
+  resource_group_name = azurerm_resource_group.backend_rg.name
+
+  location                 = azurerm_resource_group.backend_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
+resource "azurerm_storage_container" "cloud_shell" {
+  name                  = "cloud-shell"
+  storage_account_name  = azurerm_storage_account.misc_storage.name
+  container_access_type = "blob"
+}
