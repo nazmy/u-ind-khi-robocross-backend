7bc5c5a08b25bfda44d39b07408b8a0638264226
Nazmy Achmad <nazmy.achmad@unity3.com>
2023-08-16 15:07:50 +0700

    [khi-388] added whitelist ip to mongoDB and enabled client cert on app service

--- infrastructure/terraform/dev/locals.tf
+++ infrastructure/terraform/dev/locals.tf
@@ -7,4 +7,17 @@ locals {
   }
 
   dotnet_version = "7.0"
+
+  # IP Range Filter here is to allow azure portal access
+  cosmosdb_ip_range_azure = [
+    "104.42.195.92/32",
+    "40.76.54.131/32",
+    "52.176.6.30/32",
+    "52.169.50.45/32",
+    "52.187.184.26/32"
+  ]
+
+  ip_whitelist = [
+    "194.223.135.82/32"
+  ]
 }
