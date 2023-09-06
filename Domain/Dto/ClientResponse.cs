ced8d198f2aa12db5c19729f7b0d7353f1eae10d
Nazmy Achmad <nazmy.achmad@unity3.com>
2023-09-06 17:35:24 +0800

    [khi-363] added ClientType and enumeration

--- Domain/Dto/ClientResponse.cs
+++ Domain/Dto/ClientResponse.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Text.Json.Serialization;
 using domain.Dto;
+using Domain.Helper;
 
 namespace Domain.Dto
 {
@@ -12,6 +13,9 @@ namespace Domain.Dto
         public string EmailAddress { get; set; }
 
         public string PhoneNumber { get; set; }
+        
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public string ClientType { get; set; }
 	}
 
 
---
+++ Domain/Helper/ClientTypeEnum.cs
@@ -0,0 +1,11 @@
+using System.Runtime.Serialization;
+
+namespace Domain.Helper;
+
+public enum ClientTypeEnum
+{ 
+        [EnumMember(Value = "Industry")]
+        Industry,  
+        [EnumMember(Value = "Service")]
+        Service
+}
