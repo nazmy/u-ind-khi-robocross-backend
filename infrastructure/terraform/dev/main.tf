06f8509a41b6230050c2690c3b66991704093b2b
Nazmy Achmad <nazmy.achmad@unity3d.com>
2023-12-11 12:28:07 +0800

    Create tmp auth app service (#96)
    * added app service for auth version
    
    Co-authored-by: Nazmy Achmad <nazmy.achmad@unity3.com>

--- infrastructure/terraform/dev/main.tf
+++ infrastructure/terraform/dev/main.tf
@@ -117,3 +117,52 @@ resource "azurerm_linux_web_app" "khirobocross-irex" {
     }
   }
 }
+
+//temp for auth
+resource "azurerm_linux_web_app" "khirobocross-auth" {
+  name                = "${var.resource_prefix}-auth-api"
+  resource_group_name = azurerm_resource_group.backend_rg.name
+  location            = azurerm_service_plan.khirobocross.location
+  service_plan_id     = azurerm_service_plan.khirobocross.id
+
+  client_certificate_enabled = false
+  client_certificate_mode    = "OptionalInteractiveUser"
+
+  virtual_network_subnet_id = azurerm_subnet.private.id
+
+  https_only = true
+
+  app_settings = {
+    "AZURE_MONGDB_DATABASENAME"      = var.azure_mongodb_databasename
+    "ASPNETCORE_ENVIRONMENT"         = var.aspnetcore_environment
+    "DOTNET_ENVIRONMENT"             = var.aspnetcore_environment
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = var.appservice_appinsight_instrumentation_key
+  }
+
+  connection_string {
+    name  = "COSMOS_DB_CONNECTION_STRING"
+    value = var.cosmo_db_connection_string
+    type  = "Custom"
+  }
+
+  connection_string {
+    name  = "KHIROBOCROSS_STORAGE_CONNECTION_STRING"
+    value = var.khirobocross_storage_connection_string
+    type  = "Custom"
+  }
+
+  connection_string {
+    name  = "KHIROBOCROSS_STORAGE_KEY"
+    value = var.khirobocross_storage_key
+    type  = "Custom"
+  }
+
+
+  site_config {
+    minimum_tls_version    = "1.2"
+    vnet_route_all_enabled = true
+    application_stack {
+      dotnet_version = local.dotnet_version
+    }
+  }
+}
